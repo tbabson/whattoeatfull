279068a7748debde7a17e712de408d2e1f0d010b
Babatunde Taiwo <tbabson20@gmail.com>
2024-09-23 15:29:35 +0100

    food update

--- client/src/components/Food.jsx
+++ client/src/components/Food.jsx
@@ -1,35 +1,48 @@
 import { Link } from "react-router-dom";
 
-const Food = () => {
-  return <div>Food</div>;
-};
+const Food = ({
+  _id,
+  image,
+  name,
+  meal,
+  country,
+  ingredients,
+  howToPrepare,
+  price,
+}) => {
+  return (
+    <div>
+      <Link key={_id} to={`/foods/${_id}`}>
+        <figure>
+          <img src={image} alt={name} />
+        </figure>
+        <div>
+          <p>{meal}</p> <p>{country}</p>
+          <h3>{name}</h3>
+          <div className="">
+            <h3>Ingredients:</h3>
 
-// const Food = ({
-//   _id,
-//   image,
-//   name,
-//   meal,
-//   country,
-//   ingredients,
-//   howToPrepare,
-//   price,
-// }) => {
-//   return (
-//     <div>
-//       <Link key={_id} to={`/foods/${_id}`}>
-//         <figure>
-//           <img src={image} alt={name} />
-//         </figure>
-//         <div>
-//           <p>{meal}</p>
-//           <h3>{name}</h3>
-//           <h5>{ingredients}</h5>
-//           <h5>{howToPrepare}</h5>
-//           <h5>{price}</h5>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// };
+            <ol>
+              {ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ol>
+          </div>
+          <div className="">
+            <h3>How to prepare:</h3>
+
+            <ol>
+              {howToPrepare.map((step, index) => (
+                <li key={index}>{step}</li>
+              ))}
+            </ol>
+
+            <h5>{price}</h5>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default Food;
--- client/src/components/FoodContainer.jsx
+++ client/src/components/FoodContainer.jsx
@@ -4,13 +4,14 @@ import { useAllFoodsContext } from "../pages/Foods";
 const FoodContainer = () => {
   const { data } = useAllFoodsContext();
   const { foods } = data;
+
   if (foods.length === 0) {
     return <h2>No food to display</h2>;
   }
   return (
     <div>
       {foods.map((food) => {
-        <Food key={food._id} {...food} />;
+        return <Food key={food._id} {...food} />;
       })}
     </div>
   );
